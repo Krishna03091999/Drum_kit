f05ddac50c14db206a6e3d3361bf10a6029ede61
Krishna03091999 <64013860+Krishna03091999@users.noreply.github.com>
2021-02-25 17:22:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+var numOfButton = document.querySelectorAll('.drum').length
+
+// detecting button press
+for (var i = 0; i < numOfButton; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    var buttonInner = this.innerHTML
+    music(buttonInner)
+    styleButton(buttonInner)
+    })
+}
+// Detecting Keyboard press
+document.addEventListener('keypress', function(e){
+    music(e.key)
+    styleButton(e.key)
+})
+
+// checking key & playing music
+function music(key){
+    switch (key) {
+        case 'w':
+            var tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play()
+            break;
+        case 'a':
+            var tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play()
+            break;
+        case 's':
+            var tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play()
+            break;
+        case 'd':
+            var tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play()
+            break;
+        case 'j':
+            var snare = new Audio('sounds/snare.mp3')
+            snare.play()
+            break;
+        case 'k':
+            var crash = new Audio('sounds/crash.mp3')
+            crash.play()
+            break;
+        case 'l':
+            var kick = new Audio('sounds/kick-bass.mp3')
+            kick.play()
+            break;
+        default:
+            console.log(key)
+            break;
+    }
+}
+
+
+
+// addEventListener('keypress', function(e){
+//     if(e.key==='w'){
+//         alert('w is pressed')
+//     }
+// })
+
+function styleButton(key){
+    var activebutton=document.querySelector('.'+key)
+    activebutton.classList.add('pressed')
+    setTimeout(function(){
+        activebutton.classList.remove('pressed')
+    }, 100)
+}
